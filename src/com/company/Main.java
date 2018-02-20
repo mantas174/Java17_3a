8f37387b8b5bdf8847084bf571fd0e5851d8514f
Mantas <mantas.muzzz@gmail.com>
2018-02-20 09:50:08 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 3a.
+        //​
+        // Parašyti programą, kuri paprašytų įvesti 5 skaičius. Baigus skaičių įvedimą, turi būti
+        //atvaizduojama įvestų skaičių suma ir visi įvesti skaičiai.
+
+
+    }
+}
